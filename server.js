b0d5d9950695bdf6a59c1cfc1a9aee07be188d0b
cansu2 <alacansu@gmail.com>
2018-05-09 15:24:35 -0600

    mong

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 let express = require('express')
 let request = require('request')
 let querystring = require('querystring')
+let bodyParser = require("body-parser");
+
+let logger = require("morgan");
+let mongoose = require("mongoose");
+
+const db = require("./models");
+
+mongoose.Promise = Promise;
+
 
 let app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 let redirect_uri = 
   process.env.REDIRECT_URI || 
@@ -16,7 +29,60 @@ app.get('/login', function(req, res) {
       scope: 'user-read-private user-read-email user-read-playback-state',
       redirect_uri
     })})
+    // console.log(res);
 })
+
+
+app.post('/login/search', function(req, res) {
+
+    db.User.create(req.body)
+    .then(function(dbUser) {
+       res.json(dbUser)
+    })
+    .catch(function(err) {
+        res.json(err)
+    })
+})
+
+
+
+app.post('/login/searchterm', function(req, res) {
+
+  console.log("req body", req.body)
+    db.Search.create(req.body)
+    .then(function(dbSearch) {
+       res.json(dbSearch)
+    })
+    .catch(function(err) {
+        res.json(err)
+    })
+})
+
+//mongo stuff
+
+const databaseUrl = 'mongodb://localhost/litDB';
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUrl)
+}
+
+const mc = mongoose.connection;
+
+mc.on('error', function(err){
+  console.log("Mongoose Error: ", err);
+});
+
+mc.once('open', function() { 
+  console.log('Mongoose connection successful.')
+}); 
+
+// app.use((req, res, next) => {
+//   console.log("What is our req.url", req.url);
+  
+//   next();
+// })
 
 app.get('/callback', function(req, res) {
   let code = req.query.code || null
